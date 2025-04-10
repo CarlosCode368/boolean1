7a4bd719fd751e16c8abb80a2a18b1ca4c1b9ddf
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-10 10:34:02 -0700

    booleans

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void ageCheck(int age) {
+        int LIMIT = 18;
+        boolean isAdult = age >= LIMIT;
+        System.out.println("IS ADULT:" + isAdult);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        ageCheck(8);
+        //COMPARISON OPERATORS
+        System.out.println(25 >= 21); //greater than or equal
+        System.out.println(25 <= 21); //less than or equal
+        System.out.println(25 < 21); //less than
+        System.out.println(25 > 21); //greater than
+        System.out.println(25 == 21); //exactly equal
+        System.out.println(25 != 21); //Not equal
+
+        boolean IsDiabetic = true;
+        boolean likesCake = true;
+        if (likesCake && !IsDiabetic) { //if likes cake AND is NOT diabetic
+            System.out.println("eats cake");
+        }
+        boolean isCat = false;
+        boolean isDog = true;
+        if (isCat || isDog) { // if is a cat OR is a dog then pet it.  || means or
+            System.out.println("Pet it");
+        }else{
+            System.out.println("Don't pet it.");
         }
     }
 }
